f310290f196ce533a5221f59eef0fe014fff1bbb
Umanga The Great <redhat.07.0001@gmail.com>
2026-09-01 14:10:04 +0545

    3.4 Sign-in page with proper authentication

--- src/app/(root)/(tabs)/profile.tsx
+++ src/app/(root)/(tabs)/profile.tsx
@@ -1,9 +1,34 @@
-import { Text, View } from "react-native";
+import { useAuth, useUser } from "@clerk/expo";
+import { useRouter } from "expo-router";
+import { Alert, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ProfileScreen() {
+  const { user } = useUser();
+  const { signOut } = useAuth();
+  const router = useRouter();
+
+  const handleSignOut = () => {
+    Alert.alert("Sign Out", "Are you sure you want to sign-out?", [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Sign Out",
+        style: "destructive",
+        onPress: async () => {
+          await signOut();
+          router.replace("/sign-in");
+        },
+      },
+    ]);
+  };
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <SafeAreaView>
+      <TouchableOpacity>
+        <Text onPress={handleSignOut} className="text-brand-coral text-center">
+          Log Out
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,5 +10,5 @@ export default function Index() {
   if (isSignedIn) {
     return <Redirect href="./(root)/(tabs)" />;
   }
-  return <Redirect href="./(auth)/sign-up" />;
+  return <Redirect href="./(auth)/sign-in" />;
 }
